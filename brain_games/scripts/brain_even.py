10a02bc64f25c5cfb1687154ac21890edc315ead
gReen7aRRow <f.gusarov@inbox.ru>
2022-01-25 16:44:35 +0300

    second testing

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import randint
-from time import sleep
 
 
 def main():
@@ -10,37 +9,30 @@ def main():
     while name == '':
         print('May I have your name? ', end='')
         name = input()
-        sleep(1)
     print('Hello, ' + str(name) + '!')
-    sleep(1)
     print('Answer "yes" if the number is even, otherwise answer "no".')
     x = 0
     while x < 3:
-        number = int(input('Write your number: '))
+        number = randint(1, 20)
         print('Question: ' + str(number))
         answer = input('Your answer: ')
-        sleep(1)
         if number % 2 == 0:
             if answer == 'yes':
                 print('Correct!')
-                sleep(1)
                 x += 1
             else:
                 print(
                     '"' + answer + '" is wrong answer ;(. Correct answer was "yes". '
                     '\nLet\'s try again, ' + str(name))
-                sleep(1)
                 x = 0
         else:
             if answer == 'no':
                 print('Correct!')
-                sleep(1)
                 x += 1
             else:
                 print(
                     '"' + answer + '" is wrong answer ;(. Correct answer was "no". '
                     '\nLet\'s try again, ' + str(name))
-                sleep(1)
                 x = 0
     print('Congratulations,' + str(name) + '!')
 
