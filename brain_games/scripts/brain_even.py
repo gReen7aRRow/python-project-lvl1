5d100f93b2d88228e90b3b8d00767f62d8aad1aa
gReen7aRRow <f.gusarov@inbox.ru>
2022-01-25 18:38:57 +0300

    brain-evens restarting

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -12,31 +12,38 @@ def main():
     print('Hello, ' + str(name) + '!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     x = 0
-    while x < 3:
-        number = randint(1, 20)
-        print('Question: ' + str(number))
-        answer = input('Your answer: ')
-        if number % 2 == 0:
-            if answer == 'yes':
-                print('Correct!')
-                x += 1
+    while x <= 5:
+        if x < 3:
+            number = randint(1, 20)
+            print('Question: ' + str(number))
+            answer = input('Your answer: ')
+            if number % 2 == 0:
+                correct_answer = 'yes'
+                if answer == correct_answer:
+                    print('Correct!')
+                    x += 1
+                else:
+                    print(
+                        '"' + answer + '" is wrong answer ;(. '
+                        'Correct answer was "' + correct_answer + '"'
+                        '\nLet\'s try again, ' + str(name))
+                    break
             else:
-                print(
-                    '"' + answer + '" is wrong answer ;(. '
-                    'Correct answer was "yes". '
-                    '\nLet\'s try again, ' + str(name))
-                x = 0
-        else:
-            if answer == 'no':
-                print('Correct!')
-                x += 1
-            else:
-                print(
-                    '"' + answer + '" is wrong answer ;(. '
-                    'Correct answer was "no". '
-                    '\nLet\'s try again, ' + str(name))
-                x = 0
-    print('Congratulations, ' + str(name) + '!')
+                correct_answer = 'no'
+                if answer == correct_answer:
+                    print('Correct!')
+                    x += 1
+                else:
+                    print(
+                        '"' + answer + '" is wrong answer ;(. '
+                        'Correct answer was "' + correct_answer + '"'
+                        '\nLet\'s try again, ' + str(name))
+                    break
+        if x < 3:
+            pass
+        elif x == 3:
+            print('Congratulations, ' + str(name) + '!')
+            break
 
 
 if __name__ == '__main__':
