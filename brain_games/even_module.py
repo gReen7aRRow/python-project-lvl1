efce3e607ca7ce1e0c75c402534f40f1be61c88f
gReen7aRRow <f.gusarov@inbox.ru>
2022-01-26 13:10:04 +0300

    test too complex in brain-even

--- brain_games/even_module.py
+++ brain_games/even_module.py
@@ -13,35 +13,33 @@ def game():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     x = 0
     while x <= 5:
-        if x < 3:
-            number = randint(1, 20)
-            print('Question: ' + str(number))
-            answer = input('Your answer: ')
-            if number % 2 == 0:
-                correct_answer = 'yes'
-                if answer == correct_answer:
-                    print('Correct!')
-                    x += 1
-                else:
-                    print(
-                        '"' + answer + '" is wrong answer ;(. '
-                        'Correct answer was "' + correct_answer + '"'
-                        '\nLet\'s try again, ' + str(name) + '!')
-                    break
+        number = randint(1, 20)
+        print('Question: ' + str(number))
+        answer = input('Your answer: ')
+        if number % 2 == 0:
+            correct_answer = 'yes'
+            if answer == correct_answer:
+                print('Correct!')
+                x += 1
             else:
-                correct_answer = 'no'
-                if answer == correct_answer:
-                    print('Correct!')
-                    x += 1
-                else:
-                    print(
-                        '"' + answer + '" is wrong answer ;(. '
-                        'Correct answer was "' + correct_answer + '"'
-                        '\nLet\'s try again, ' + str(name) + '!')
-                    break
+                print(
+                    '"' + answer + '" is wrong answer ;(. '
+                    'Correct answer was "' + correct_answer + '"'
+                    '\nLet\'s try again, ' + str(name) + '!')
+                break
+        else:
+            correct_answer = 'no'
+            if answer == correct_answer:
+                print('Correct!')
+                x += 1
+            else:
+                print(
+                    '"' + answer + '" is wrong answer ;(. '
+                    'Correct answer was "' + correct_answer + '"'
+                    '\nLet\'s try again, ' + str(name) + '!')
+                break
         if x < 3:
             pass
         elif x == 3:
             print('Congratulations, ' + str(name) + '!')
             break
-
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from random import randint
 from brain_games.even_module import game
 
 
