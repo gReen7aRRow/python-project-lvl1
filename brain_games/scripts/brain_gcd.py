e3d1a88fe2378d22feeeb6980a594ab6250abaff
gReen7aRRow <f.gusarov@inbox.ru>
2022-01-31 11:48:03 +0300

    complete brain-gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -13,29 +13,17 @@ def main():
     x = 0
     while x < 3:
         number1 = randint(1, 50)
-        number2 = randint(1, 50)
+        correct_answer = number2 = randint(1, number1)
         print('Question: ' + str(number1) + ' ' + str(number2))
         answer = int(input('Your answer: '))
-        if number1 > number2:
-            correct_answer = number2
-            while correct_answer > 0:
-                if number1 % correct_answer == number2 % correct_answer:
-                    if number1 % correct_answer == 0:
-                        break
-                    else:
-                        correct_answer -= 1
+        while correct_answer > 0:
+            if number1 % correct_answer == number2 % correct_answer:
+                if number1 % correct_answer == 0:
+                    break
                 else:
                     correct_answer -= 1
-        else:
-            correct_answer = number1
-            while correct_answer > 0:
-                if number1 % correct_answer == number2 % correct_answer:
-                    if number1 % correct_answer == 0:
-                        break
-                    else:
-                        correct_answer -= 1
-                else:
-                    correct_answer -= 1
+            else:
+                correct_answer -= 1
         if answer == correct_answer:
             print('Correct!')
             x += 1
@@ -45,9 +33,7 @@ def main():
                 'Correct answer was ' + '\'' + str(correct_answer) + '\'. '
                 '\nLet\'s try again, ' + str(name) + '!')
             break
-        if x < 3:
-            pass
-        elif x == 3:
+        if x == 3:
             print('Congratulations, ' + str(name) + '!')
             break
 
