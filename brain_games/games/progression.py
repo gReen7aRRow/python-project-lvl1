ebbb6694ec77d55ad869a76fbf78ba68cff96954
gReen7aRRow <f.gusarov@inbox.ru>
2022-02-26 16:15:13 +0300

    refactoring brain-progression v4

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,19 +6,21 @@ DESCRIPTION = 'What number is missing in the progression? '
 def get_question_and_answer():
     long = randint(7, 12)
     step = randint(3, 10)
-    first_number = randint(5, 20)
-    correct_answer, progression = generate_progression(
-        long, step, first_number)
-    question = str(progression)
+    element = randint(5, 20)
+    correct_answer, question = generate_progression(
+        long, step, element)
     return correct_answer, question
 
 
-def generate_progression(long, step, first_number):
-    progression = []
-    for i in range(1, long + 1):
-        progression.append(first_number)
-        first_number += step
+def generate_progression(long, step, element):
+    progression = ''
     enigma = randint(5, long - 1)
-    correct_answer = progression[enigma]
-    progression[enigma] = '..'
+    for i in range(1, long + 1):
+        if i == enigma:
+            progression = " ".join([progression, '..'])
+            correct_answer = element
+            element += step
+        else:
+            progression = " ".join([progression, str(element)])
+            element += step
     return correct_answer, progression
